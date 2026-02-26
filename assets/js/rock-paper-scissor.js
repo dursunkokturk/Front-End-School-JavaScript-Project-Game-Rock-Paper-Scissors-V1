c9faed96f667ed1e967eea460e7a5bb09d78a89e
Dursun Kokturk <dursunserefkokturk@gmail.com>
2026-02-26 12:12:01 +0300

    assets klasoru icindeki rock-paper-scissor.js dosyasinda butonlarin calismasi while dongusunden cikarildi

--- assets/js/rock-paper-scissor.js
+++ assets/js/rock-paper-scissor.js
@@ -1,23 +1,45 @@
 let userScore = 0;
 let computerScore = 0;
 
+/* Bilgisayar Tercihini Duzenliyoruz */
+let computerSelect = ["taş", "kağıt", "makas"].at(Math.floor(Math.random() * 3));
+
+function rock() {
+  let r = rockButton.innerHTML;
+  console.log(r);
+}
+
+function paper() {
+  let p = paperButton.innerHTML;
+  console.log(p);
+}
+
+function scissor() {
+  let s = scissorButton.innerHTML;
+  console.log(s);
+}
+
+function gameEnd() {
+  let ge = gameEndButton.innerHTML;
+  console.log(ge);
+}
+
+
 while (true) {
 
   /* Kullanicidan Tercihi Aliyoruz */
   /* let userPreference = prompt("Tercihinizi Giriniz (taş / kağıt / makas / çıkış)"); */
 
-  /* Bilgisayar Tercihini Duzenliyoruz */
-  let computerSelect = ["taş", "kağıt", "makas"].at(Math.floor(Math.random() * 3));
 
   /* Oyun Baslamadan Yada Oyun Esnasinda Oyunu Bitirme */
-  if (userPreference === "çıkış"){
+  if (userPreference === "çıkış") {
     alert("Oyun Bitti");
     console.log(`Kullanıcı Puanı : ${userScore} Bilgisayar Puanı : ${computerScore}`);
     break;
   }
 
   /* Kullaniciya Sunulan Tercihlerin Disinda Bir Tercih Girilmesi Durumu */
-  if(!["taş", "kağıt", "makas"].includes(userPreference)){
+  if (!["taş", "kağıt", "makas"].includes(userPreference)) {
     alert("Geçersiz Hamle");
     continue;
   }
